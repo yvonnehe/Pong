49577e1feb09069764dd8d9b60f3e5ad9f8841f7
Yvonne H <yvonnehelland@outlook.com>
2021-06-19 18:46:10 +0200

    Selects elements

---
+++ script.js
@@ -0,0 +1,11 @@
+const player1 = document.querySelector(".player1");
+const player2 = document.querySelector(".player2");
+const ball = document.querySelector(".ball");
+const gameContainer = document.querySelector(".gamecontainer");
+const startButton = document.querySelector(".startbutton");
+
+//function startGame
+
+//knapp.onclick {
+//    startGame()
+//}
