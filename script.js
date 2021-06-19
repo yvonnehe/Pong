ed12b85588f86460715fd6fd932cf08684bb25ab
Yvonne H <yvonnehelland@outlook.com>
2021-06-19 23:37:56 +0200

    Adds function to toggle themes

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 const player1 = document.querySelector(".player1");
 const player2 = document.querySelector(".player2");
 const ball = document.querySelector(".ball");
+const layout = document.querySelector(".layout");
 const gameContainer = document.querySelector(".gamecontainer");
 const startButton = document.querySelector(".startbutton");
+const themeButton = document.querySelector(".themebutton");
+
+function changeTheme() {
+  ball.classList.toggle("jellowarball");
+  layout.classList.toggle("jellowarlayout");
+  themeButton.classList.toggle("jellowarbtn");
+  if (themeButton.innerHTML === "Jello War") {
+    themeButton.innerHTML = "Pet the cat";
+  } else {
+    themeButton.innerHTML = "Jello War";
+  }
+}
+
+themeButton.addEventListener("click", changeTheme);
 
 //function startGame
 
