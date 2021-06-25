9798239ec7e343f22e68adbea13caf20dd7995ed
Yvonne H <yvonnehelland@outlook.com>
2021-06-25 16:27:52 +0200

    Complete re-write for 1 player code

--- js/game.js
+++ js/game.js
@@ -1,4 +1,3 @@
-const ball2 = document.querySelector(".ball");
 const layout = document.querySelector(".layout");
 const startButton = document.querySelector(".startbutton");
 const startButton2 = document.querySelector(".startbutton2");
@@ -43,14 +42,17 @@ let themes = [
 ];
 
 let activeTheme = 0;
+let onePlayer = false;
+let AICanMove = true;
 
 let ball = {
-  x: canvas.width / 2 - 50,
-  y: canvas.height / 2 - 50,
-  velocityX: -3, //olga speed
+  posX: canvas.width / 2 - 50,
+  posY: canvas.height / 2 - 50,
+  velocityX: -5, //olga speed
   velocityY: 0,
   image: "",
 };
+let oldBallY = canvas.height / 2 - 50;
 
 const players = [
   {
@@ -105,8 +107,8 @@ function drawPawn(pawn) {
 function drawBall() {
   ctx.drawImage(
     themes[activeTheme].pawn.pawn,
-    ball.x + ball.velocityX,
-    ball.y + ball.velocityY,
+    ball.posX + ball.velocityX,
+    ball.posY + ball.velocityY,
     80,
     80
   );
@@ -167,39 +169,39 @@ function checkIfWon() {
 }
 
 function checkIfImpact() {
-  if (ball.x <= 0) {
+  if (ball.posX <= 0) {
     ball.velocityX = Math.random() > 0.5 ? -1.5 : 1.5;
     ball.velocityY = Math.random() > 0.5 ? Math.random() : -Math.random();
     players[0].posY = 200;
     players[1].posY = 200;
-    (ball.x = 250), (ball.y = 250); //center
+    (ball.posX = 250), (ball.posY = 250); //center
     players[1].score1++;
     checkIfWon();
-  } else if (ball.x > canvas.width) {
+  } else if (ball.posX > canvas.width) {
     ball.velocityX = Math.random() > 0.5 ? -1.5 : 1.5;
     ball.velocityY = Math.random() > 0.5 ? Math.random() : -Math.random();
     players[0].posY = 200;
     players[1].posY = 200;
-    (ball.x = 250), (ball.y = 250); //center
+    (ball.posX = 250), (ball.posY = 250); //center
     players[0].score0++;
     checkIfWon();
-  } else if (ball.y <= 0) {
+  } else if (ball.posY <= 0) {
     ball.velocityY = -ball.velocityY;
-  } else if (ball.y >= canvas.height - 80) {
+  } else if (ball.posY >= canvas.height - 80) {
     ball.velocityY = -ball.velocityY;
   } else if (
-    ball.x <= 31 &&
-    ball.y >= players[0].posY - 70 &&
-    ball.y <= players[0].posY + 70
+    ball.posX <= 31 &&
+    ball.posY >= players[0].posY - 70 &&
+    ball.posY <= players[0].posY + 70
   ) {
-    ball.velocityY = (ball.y - players[0].posY) * 0.05;
+    ball.velocityY = (ball.posY - players[0].posY) * 0.05;
     ball.velocityX = -ball.velocityX; //olga speed
   } else if (
-    ball.x >= 420 &&
-    ball.y >= players[1].posY - 70 &&
-    ball.y <= players[1].posY + 70
+    ball.posX >= 420 &&
+    ball.posY >= players[1].posY - 70 &&
+    ball.posY <= players[1].posY + 70
   ) {
-    ball.velocityY = (ball.y - players[1].posY) * 0.05;
+    ball.velocityY = (ball.posY - players[1].posY) * 0.05;
     ball.velocityX = -ball.velocityX; //olga speed
   }
 }
@@ -235,23 +237,51 @@ function movePlayers(e) {
 window.addEventListener("keydown", (e) => movePlayers(e));
 
 function updateBall() {
-  ball.x += ball.velocityX;
-  ball.y += ball.velocityY;
+  ball.posX += ball.velocityX;
+  ball.posY += ball.velocityY;
+  oldBallY = ball.posX += ball.velocityX;
 }
 
 /* Start game */
 startButton.addEventListener("click", addPlayer2);
+startButton2.addEventListener("click", () => {
+  onePlayer = true;
+  initializeGame();
+});
+
+function moveAI() {
+  /* AI Computer */
+  let computerLevel = 1;
+  //console.log(ball.posY)
+  if (AICanMove && Math.abs(players[0].posY - ball.posY) > 15) {
+    setTimeout(() => {
+      players[0].posY = (ball.posY - 70 / 2) * (Math.random() * 0.6 + 1);
+    }, 2000 / computerLevel);
+    console.log(players[0].posY);
+    AICanMove = false;
+    setTimeout(() => {
+      AICanMove = true;
+    }, 400 / computerLevel);
+  }
+}
 
 function renderGame() {
+  if (onePlayer) {
+    moveAI();
+  }
+
   ctx.clearRect(0, 0, 9999, 9999);
   drawGame(themes[0]);
   drawBall();
   updateBall();
   checkIfImpact();
+
   window.requestAnimationFrame(renderGame);
 }
 
 function initializeGame() {
+  startButton.style.display = "none";
+  startButton2.style.display = "none";
   saveName2();
   createPawn(themes);
   createPlayers();
@@ -276,143 +306,15 @@ function saveName2() {
 
 /* Restart game */
 function restartGame() {
-  ball = { ...ball, x: 250, y: 250, velocityX: 0, velocityY: 0 };
+  ball = { ...ball, posX: 250, posY: 250, velocityX: 0, velocityY: 0 };
 
-  players = { ...players, score0: 0, score1: 0 };
+  players.map((player) => {
+    return { ...player, score0: 0, score1: 0 };
+  });
   //startButton.style.display = "inline-block";
 }
 
-/* COMPUTER VS PLAYER CODE */
-const computerAndPlayer = [
-  {
-    posX: 0,
-    posY: canvas.height / 2 - 50,
-    image: "./images/Hand.png",
-    score0: 0,
-  },
-  {
-    posX: canvas.width - 31,
-    posY: canvas.height / 2 - 50,
-    image: "./images/Hand2.png",
-    score1: 0,
-  },
-];
-
-function createPlayers2() {
-  computerAndPlayer.forEach((player) => {
-    let image = new Image();
-    image.setAttribute("src", player.image);
-    player.pawn = image;
-  });
-}
-
-function drawPlayers2(computerAndPlayer) {
-  computerAndPlayer.forEach((player) => {
-    ctx.drawImage(player.pawn, player.posX, player.posY, 31, 70);
-    //console.log(player.pawn, player.posX, player.posY, 31, 70);
-  });
-}
-
-function drawGame2(theme) {
-  ctx.clearRect(0, 0, 9999, 9999);
-  drawPlayers2(players);
-  drawScore();
-}
-
-/* AI Computer */
-let computerLevel = 0.1;
-
-computerAndPlayer[0].y +=
-  (ball.y - (computerAndPlayer[0].y + computerAndPlayer[0].height / 2)) *
-  computerLevel;
-
-/* Game code */
-function checkIfWon2() {
-  if (computerAndPlayer[0].score0 >= 11) {
-    alert("Player 1 wins!");
-    ctx.clearRect(0, 0, 9999, 9999);
-    restartGame();
-  } else if (computerAndPlayer[1].score1 >= 11) {
-    alert("Player 2 wins!");
-    ctx.clearRect(0, 0, 9999, 9999);
-    restartGame();
-  }
-}
-
-function checkIfImpact2() {
-  if (ball.x <= 0) {
-    ball.velocityX = Math.random() > 0.5 ? -1.5 : 1.5;
-    ball.velocityY = Math.random() > 0.5 ? Math.random() : -Math.random();
-    computerAndPlayer[0].posY = 200;
-    computerAndPlayer[1].posY = 200;
-    (ball.x = 250), (ball.y = 250); //center
-    computerAndPlayer[1].score1++;
-    checkIfWon2();
-  } else if (ball.x > canvas.width) {
-    ball.velocityX = Math.random() > 0.5 ? -1.5 : 1.5;
-    ball.velocityY = Math.random() > 0.5 ? Math.random() : -Math.random();
-    computerAndPlayer[0].posY = 200;
-    computerAndPlayer[1].posY = 200;
-    (ball.x = 250), (ball.y = 250); //center
-    computerAndPlayer[0].score0++;
-    checkIfWon2();
-  } else if (ball.y <= 0) {
-    ball.velocityY = -ball.velocityY;
-  } else if (ball.y >= canvas.height - 80) {
-    ball.velocityY = -ball.velocityY;
-  } else if (
-    ball.x <= 31 &&
-    ball.y >= computerAndPlayer[0].posY - 70 &&
-    ball.y <= computerAndPlayer[0].posY + 70
-  ) {
-    ball.velocityY = (ball.y - computerAndPlayer[0].posY) * 0.05;
-    ball.velocityX = -ball.velocityX; //olga speed
-  } else if (
-    ball.x >= 420 &&
-    ball.y >= computerAndPlayer[1].posY - 70 &&
-    ball.y <= computerAndPlayer[1].posY + 70
-  ) {
-    ball.velocityY = (ball.y - computerAndPlayer[1].posY) * 0.05;
-    ball.velocityX = -ball.velocityX; //olga speed
-  }
-}
-
-function movePlayers2(e) {
-  if (e.code === "ArrowUp" || e.code === "ArrowDown") {
-    e.preventDefault();
-    switch (e.code) {
-      case "ArrowUp":
-        players[1].posY -= 30;
-        break;
-
-      case "ArrowDown":
-        players[1].posY += 30;
-        break;
-    }
-  }
-}
-
-/* Start game 2 */
-startButton2.addEventListener("click", initializeGame2);
-
-function renderGame2() {
-  ctx.clearRect(0, 0, 9999, 9999);
-  drawGame2(themes[0]);
-  drawBall();
-  updateBall();
-  checkIfImpact2();
-  window.requestAnimationFrame(renderGame);
-}
-
-function initializeGame2() {
-  console.log("fuck u");
-  startButton.style.display = "none";
-  startButton2.style.display = "none";
-  createPawn(themes);
-  createPlayers2();
-  renderGame2();
-  window.addEventListener("keydown", (e) => movePlayers2(e));
-}
+/*
 
 /* Change theme function not in use
 function changeTheme() {
